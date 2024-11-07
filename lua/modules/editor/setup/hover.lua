7ad2d4db3aad244ea353773e81142f7164aa36d6
Kevin Silvester <kevinsilvester05@gmail.com>
2024-11-07 02:05:40 +0000

    chore(hover): removed commented lines

--- lua/modules/editor/setup/hover.lua
+++ lua/modules/editor/setup/hover.lua
@@ -1,5 +1,4 @@
 ---@diagnostic disable: missing-parameter
--- local m = require('core.mapper')
 local M = {}
 
 M.config = function()
@@ -16,51 +15,7 @@ M.config = function()
       preview_opts = {
          border = 'rounded',
       },
-      --             -- Whether the contents of a currently open hover window should be moved
-      --             -- to a :h preview-window when pressing the hover keymap.
-      --             preview_window = false,
-      --             title = true,
-      --             mouse_providers = {
-      --                 'LSP'
-      --             },
-      --             mouse_delay = 1000
-      --         }
-
-      --         -- Setup keymaps
-      --         vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
-      --         vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
-      --         vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end, {desc = "hover.nvim (previous source)"})
-      --         vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
-
-      --         -- Mouse support
-      --         vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
-      --         vim.o.mousemoveevent = true
-      -- end)
    })
-
-   -- m.nmap({
-   --    { 'K', require('hover').hover, m.opts(m.silent, m.noremap, m.nowait, 'hover.nvim') },
-
-   --    {
-   --       'gK',
-   --       require('hover').hover_select,
-   --       m.opts(m.silent, m.noremap, m.nowait, 'hover.nvim (select)'),
-   --    },
-   --    {
-   --       'H',
-   --       function()
-   --          require('hover').hover_switch('previous')
-   --       end,
-   --       m.opts(m.silent, m.noremap, m.nowait, 'hover.nvim (previous source)'),
-   --    },
-   --    {
-   --       'L',
-   --       function()
-   --          require('hover').hover_switch('next')
-   --       end,
-   --       m.opts(m.silent, m.noremap, m.nowait, 'hover.nvim (next source)'),
-   --    },
-   -- })
 end
 
 M.keys = {
